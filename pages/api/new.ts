1fad93c1706651448e36f401fd9e86a9f86c1f9e
freieslebenadam <adam.fre@seznam.cz>
2022-05-07 22:48:32 +0200

    Async fetch fix

--- pages/api/new.ts
+++ pages/api/new.ts
@@ -1,11 +1,11 @@
 import { insertNewShortlink } from '@utils/supabase'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler({ body: { original_link, short_link }, headers: { host }}: NextApiRequest, res: NextApiResponse<any>) {
+export default async function handler({ body: { original_link, short_link }, headers: { host }}: NextApiRequest, res: NextApiResponse<any>) {
   if (original_link && short_link) {
     const shortlink = `${host}/${short_link}`
-    const response = insertNewShortlink(original_link, short_link)
-    return res.status(200).json({response, short_link: shortlink})
+    const response = await insertNewShortlink(original_link, short_link)
+    return res.status(200).json({ response, short_link: shortlink})
   }
   
   return res.status(400)
